2ae65e9602bbb8c456ceb3a105a32a1618166472
jakelee437 <lee507@wearescpps.org>
2024-12-18 19:52:21 +0000

    finished

--- projects/runtime/js/levelData.js
+++ projects/runtime/js/levelData.js
@@ -5,6 +5,7 @@ var makeLevelData = function (window) {
     // some useful constants
     var groundY = game.groundY;
 
+
     // this data will allow us to define all of the
     // behavior of our game
 
@@ -15,19 +16,59 @@ var makeLevelData = function (window) {
         number: 1,
         speed: -3,
         gameItems: [
-          { type: "sawblade", x: 400, y: groundY },
-          { type: "sawblade", x: 600, y: groundY },
-          { type: "sawblade", x: 900, y: groundY },
+          { type: "knight", x: 800, speed: 6},
+          { type: "knight", x: 1500, speed: 7},
+          { type: "knight", x: 2200, speed: 7},
+          { type: "knight", x: 2900, speed: 6},
+          { type: "knight", x: 3500, speed: 5},
+          { type: "knight", x: 4400, speed: 6},
+          { type: "knight", x: 5700, speed: 7},
+          { type: "knight", x: 6600, speed: 5},
+          { type: "knight", x: 7100, speed: 7},
+
+          { type: "bat", x: 500, y: 291},
+          { type: "bat", x: 900, y: 220},
+          { type: "bat", x: 1500, y: 220},
+          { type: "bat", x: 1900, y: 291},
+          { type: "bat", x: 2500, y: 220},
+          { type: "bat", x: 2900, y: 291},
+          { type: "bat", x: 3700, y: 291},
+          { type: "bat", x: 4600, y: 220},
+
+          { type: "coin", x: 3700, y: 270},
+
+          { type: "levelEnd", x: 10000, y: 260},
         ],
+
+
       },
       {
         name: "Robot Rampage",
         number: 2,
         speed: -3,
         gameItems: [
-          { type: "sawblade", x: 400, y: groundY },
-          { type: "sawblade", x: 600, y: groundY },
-          { type: "sawblade", x: 900, y: groundY },
+          { type: "knight", x: 1800, speed: 8},
+          { type: "knight", x: 11300, speed: 9},
+          { type: "knight", x: 12000, speed: 9},
+          { type: "knight", x: 12700, speed: 8},
+          { type: "knight", x: 13500, speed: 7},
+          { type: "knight", x: 14400, speed: 8},
+          { type: "knight", x: 15600, speed: 9},
+          { type: "knight", x: 16500, speed: 7},
+          { type: "knight", x: 17000, speed: 9},
+
+          { type: "bat", x: 1500, y: 291},
+          { type: "bat", x: 1900, y: 220},
+          { type: "bat", x: 11500, y: 220},
+          { type: "bat", x: 11900, y: 291},
+          { type: "bat", x: 12500, y: 220},
+          { type: "bat", x: 12900, y: 291},
+          { type: "bat", x: 13700, y: 291},
+          { type: "bat", x: 14600, y: 220},
+
+          { type: "coin", x: 13700, y: 270},
+
+          { type: "levelEnd", x: 110000, y: 260},
         ],
       },
     ];
--- projects/runtime/js/runLevels.js
+++ projects/runtime/js/runLevels.js
@@ -14,7 +14,7 @@ var runLevels = function (window) {
     var levelData = window.opspark.levelData;
 
     // set this to true or false depending on if you want to see hitzones
-    game.setDebugMode(true);
+    game.setDebugMode(false);
 
     // TODOs 5 through 11 go here
     // BEGIN EDITING YOUR CODE HERE
@@ -36,14 +36,14 @@ var runLevels = function (window) {
       batHitZone.addChild(batImage);
     }
 
-    createBat(500,291);
-    createBat(900,220);
-    createBat(1500,220);
-    createBat(1900,291);
-    createBat(2500,220);
-    createBat(2900,291);
-    createBat(3700,291);
-    createBat(4600,220);
+    //createBat(500,291);
+    //createBat(900,220);
+    //createBat(1500,220);
+    //createBat(1900,291);
+    //createBat(2500,220);
+    //createBat(2900,291);
+    //createBat(3700,291);
+    //createBat(4600,220);
 
     function createKnight(x,speed){
       var enemy = game.createGameItem("enemy", 35);
@@ -70,48 +70,90 @@ var runLevels = function (window) {
       enemy.addChild(knight);
     }
 
-    createKnight(800,6);
-    createKnight(1500,7);
-    createKnight(2200,7);
-    createKnight(2900,6);
-    createKnight(3700,5);
-    createKnight(4400,6);
-    createKnight(5700,7);
-    createKnight(6600,5);
-    createKnight(7100,7);
+    //createKnight(800,6);
+    //createKnight(1500,7);
+    //createKnight(2200,7);
+    //createKnight(2900,6);
+    //createKnight(3700,5);
+    //createKnight(4400,6);
+    //createKnight(5700,7);
+    //createKnight(6600,5);
+    //createKnight(7100,7);
 
     function createCoin(x,y){
-      var reward = game.createGameItem("reward", 25);
-      reward.x = x;
-      reward.y = y;
-      game.addGameItem(reward);
+      var enemy = game.createGameItem("enemy", 25);
+      enemy.x = x;
+      enemy.y = y;
+      enemy.velocityX = -4
+      game.addGameItem(enemy);
 
-      reward.onPlayerCollision = function () {
+      enemy.onPlayerCollision = function () {
         game.changeIntegrity(+25);
-        reward.fadeOut();
+        enemy.fadeOut();
       };
 
-      reward.onProjectileCollision = function () {
+      enemy.onProjectileCollision = function () {
         game.increaseScore(100);
-        reward.fadeOut();
+        enemy.fadeOut();
 
       }
 
       var coin = draw.bitmap("img/coin.png");
-      coin.x = 0
-      coin.y = 0
-      coin.scaleX = 1
-      coin.scaleY = 1
+      coin.x = -28;
+      coin.y = -28;
+      coin.scaleX = 0.1;
+      coin.scaleY = 0.1;
       enemy.addChild(coin);
     }
 
-    createCoin(600,400);
+    //createCoin(3700,270);
     
+    function createLevelEnd(x,y){
+      var enemy = game.createGameItem("enemy", 40);
+      enemy.x = x;
+      enemy.y = y;
+      enemy.velocityX = -4
+      game.addGameItem(enemy);
+
+      enemy.onPlayerCollision = function () {
+        game.startLevel(2);
+      };
+
+      enemy.onProjectileCollision = function () {
+        game.startLevel(2);
+      }
+
+      var flagpole = draw.bitmap("img/flagpole.png");
+      flagpole.x = -100;
+      flagpole.y = -222;
+      flagpole.scaleX = 1;
+      flagpole.scaleY = 1;
+      enemy.addChild(flagpole);
+    }
+
+    //createLevelEnd(10000,260);
 
     function startLevel() {
       // TODO 13 goes below here
 
-
+      var level = levelData[currentLevel];
+      var levelObjects = level.gameItems;
+      
+      for (var i = 0; i < level.gameItems.length; i++) {
+        var eachElement = levelObjects[i];
+        if(eachElement.type === "knight"){
+          createKnight(eachElement.x,eachElement.speed);
+        }
+        else if(eachElement.type === "bat"){
+          createBat(eachElement.x,eachElement.y);
+        }
+        else if(eachElement.type === "coin"){
+          createCoin(eachElement.x,eachElement.y);
+        }
+        else if(eachElement.type === "levelEnd"){
+          createLevelEnd(eachElement.x,eachElement.y);
+        }
+      }
 
       //////////////////////////////////////////////
       // DO NOT EDIT CODE BELOW HERE
